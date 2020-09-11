f04c13d7fa56cb05323a2984112548709564cdd5
Nidhey Nitin Indurkar <46122307+nidhey27@users.noreply.github.com>
2020-09-11 15:06:10 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include 'navbar.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smart Inventory Management</title>
+</head>
+<body>
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <h1>Simple Sidebar</h1>
+            <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
+            <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+        </div>
+    </div> <!-- /#page-content-wrapper -->
+</div> <!-- /#wrapper -->
+</body>
+</html>
